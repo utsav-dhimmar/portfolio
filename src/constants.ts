71a1e1f5f9eafcbac08446b3e051ccbbfad6c7c7
utsav-dhimmar <utsavdhimmar987@gmail.com>
2026-01-04 16:17:47 +0530

    add: python and fastapi in skills

--- src/constants.ts
+++ src/constants.ts
@@ -1,40 +1,42 @@
 export const INFO = {
-	name: "Utsav Dhimmar",
-	GITHUB_PROFILE_URL: "https://avatars.githubusercontent.com/u/136555325?v=4",
-	RESUME_LINK:
-		"https://drive.google.com/file/d/1E1_XU7R78KIcRyrNha5W1y4pjTfu_86u/view",
+  name: "Utsav Dhimmar",
+  GITHUB_PROFILE_URL: "https://avatars.githubusercontent.com/u/136555325?v=4",
+  RESUME_LINK:
+    "https://drive.google.com/file/d/1E1_XU7R78KIcRyrNha5W1y4pjTfu_86u/view",
 };
 
 export const SKILLS = [
-	"JavaScript",
-	"TypeScript",
-	"React.js",
-	"Next.js",
-	"Node.js",
-	"Express.js",
-	"MongoDB",
-	"PostgreSQL",
-	"Docker",
-	"Redis",
-	// "CSS",
-	// "Software Design",
-	// "UI/UX Design",
+  "Python",
+  "JavaScript",
+  "TypeScript",
+  "FastAPI",
+  "Node.js",
+  "Express.js",
+  "PostgreSQL",
+  "Docker",
+  "Redis",
+  "MongoDB",
+  "Next.js",
+  "React.js",
+  // "CSS",
+  // "Software Design",
+  // "UI/UX Design",
 ];
 
 export const LINKS = [
-	{
-		title: "Linkdin",
-		href: "https://www.linkedin.com/in/utsav-dhimmar/",
-		isDisplayed: true,
-	},
-	{
-		title: "Github",
-		href: "https://github.com/utsav-dhimmar/",
-		isDisplayed: true,
-	},
-	{
-		title: "X (Formerly Twitter)",
-		href: "https://x.com/utsav_dhimmar",
-		isDisplayed: true,
-	},
+  {
+    title: "Linkdin",
+    href: "https://www.linkedin.com/in/utsav-dhimmar/",
+    isDisplayed: true,
+  },
+  {
+    title: "Github",
+    href: "https://github.com/utsav-dhimmar/",
+    isDisplayed: true,
+  },
+  {
+    title: "X (Formerly Twitter)",
+    href: "https://x.com/utsav_dhimmar",
+    isDisplayed: true,
+  },
 ];
